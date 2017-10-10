089f1b94ace8f7040972ccdf5285f704d4071e44
Austin Kim <ask199416@gmail.com>
2017-10-10 20:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, customer) {
   katzDeliLine.push(customer)
-  return `Welcome, ${customer}. You are number ${katzDeliLine.length + 1} in line.``
+  return `Welcome, ${customer}. You are number ${katzDeliLine.length + 1} in line.`
 }
