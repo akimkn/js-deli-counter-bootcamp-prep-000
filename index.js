c4518117875375a119dd7bd1ee771ec8117c4f4f
Austin Kim <ask199416@gmail.com>
2017-10-10 21:08:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
   var result = "The line is currently: "
   if (katzDeliLine.length > 0) {
   for (var i=0; i <katzDeliLine.length; i++) {{
-    result === result + (`${i+1}"." ${katzDeliLine[i]}, `)
+    result === result + `${i+1}"." ${katzDeliLine[i]}, `
   }
   return result}}  else {return "The line is currently empty."}
 }
